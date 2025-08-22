227c2f0e2edc526e71f9c53b9fdea9de8414636b
Mykyta Shevtsov <50263589+Remonone@users.noreply.github.com>
2025-08-23 01:31:44 +0300

    Base Setup

---
+++ Runtime/Logger/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using EngineExtensions.Abstractions;
+
+namespace EngineExtensions.Logger {
+    public class Logger : ILog {
+        
+        private readonly List<ILogSink> _sinks = new();
+
+        public void AddSink(ILogSink sink) => _sinks.Add(sink);
+        
+        public void Write(LogLevel level, string message, LogContext context = null) {
+            LogEntry entry = new LogEntry(level, message, context);
+            foreach (var sink in _sinks) {
+                sink.Write(entry);
+            }
+        }
+    }
+}
---
+++ Runtime/Logger/Sinks/UnityLogSink.cs
@@ -0,0 +1,19 @@
+﻿using EngineExtensions.Abstractions;
+using UnityEngine;
+
+namespace EngineExtensions.Logger.Sinks {
+    public class UnityLogSink : ILogSink {
+        public void Write(LogEntry entry) {
+            var msg = entry.Context is { } c && !string.IsNullOrEmpty(c.Category) ? $"[{c.Category}] {entry.Message} (tick={c.Tick}, ent={c.EntityId})" : entry.Message;
+
+            switch (entry.Level) {
+                case LogLevel.DEBUG:
+                case LogLevel.TRACE:
+                case LogLevel.INFO: Debug.Log(msg); break;
+                case LogLevel.WARNING: Debug.LogWarning(msg); break;
+                case LogLevel.ERROR: 
+                case LogLevel.FATAL: Debug.LogError(msg); break;
+            }
+        }
+    }
+}
