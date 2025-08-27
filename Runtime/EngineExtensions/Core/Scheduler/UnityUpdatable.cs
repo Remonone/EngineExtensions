4db04df57e2b1d4b23940aea26c06de3984e3fa3
Mykyta Shevtsov <50263589+Remonone@users.noreply.github.com>
2025-08-28 00:38:25 +0300

    Add new engine extension packages including Input, Replay, AI Utility, and Extras; remove ISaveable abstraction

---
+++ Runtime/EngineExtensions/Core/Scheduler/UnityUpdatable.cs
@@ -0,0 +1,19 @@
+﻿using EngineExtensions.Abstractions;
+using UnityEngine;
+
+namespace EngineExtensions.Core.Scheduler {
+    public abstract class UnityUpdatable : MonoBehaviour, IUpdatable {
+        [SerializeField] private UpdatePhase _phase;
+        [SerializeField] private int _order = 0;
+
+        protected virtual void OnEnable() {
+            if(SchedulerLocator.Has) SchedulerLocator.Instance.Schedule(this, _phase, _order);
+        }
+        
+        protected virtual void OnDisable() {
+            if(SchedulerLocator.Has) SchedulerLocator.Instance.Unschedule(this, _phase);
+        }
+
+        public abstract void Tick(float delta);
+    }
+}
